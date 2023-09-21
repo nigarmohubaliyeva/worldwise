126050b85dd580e1286e8038cd42a8ede6fb019e
Nigar Mohubaliyeva <nigarmohubaliyeva@gmail.com>
2023-09-21 15:48:17 +0300

    ✅ implemented the cities list

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import Logo from "./Logo";
 import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -9,7 +10,7 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
       <Footer />
     </div>
   );
